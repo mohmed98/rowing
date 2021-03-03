af3ba91771a30d7167db5900cfe27547135fcbd8
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-03-03 18:45:10 +0200

    project setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import Home from "./pages/home";
+import RowingTypes from "./pages/rowingTypes";
+import WhyRowing from "./pages/whyRowing";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/rowingTypes", name: "RowingTypes", Component: RowingTypes },
+  { path: "/whyRowing", name: "WhyRowing", Component: WhyRowing },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Example() {
+  return (
+    <Router>
+      <>
+        <Navbar bg="light">
+          <Nav className="mx-auto">
+            {routes.map((route) => (
+              <Nav.Link
+                key={route.path}
+                as={NavLink}
+                to={route.path}
+                activeClassName="active"
+                exact
+              >
+                {route.name}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar>
+        <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Example />, rootElement);
