677b3f7bb26c9da8750a4657474f9f291d5474ab
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-03-06 00:29:19 +0200

    added animation

--- src/pages/rowingTypes.js
+++ src/pages/rowingTypes.js
@@ -1,4 +1,14 @@
+import Lottie from "react-lottie";
+import animationData from "../lotties/farmer";
 function RowingTypes() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <>
       <h1>rowingTypes</h1>
@@ -9,6 +19,9 @@ function RowingTypes() {
         nulla, eleifend eget dui faucibus, facilisis aliquet ante. Suspendisse
         sollicitudin nibh lacus, ut bibendum risus elementum a.
       </p>
+      <div>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
     </>
   );
 }
